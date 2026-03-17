1ba74e577eba315c11c3067eeacde08b13ef1f71
Mubbe <88598179+Mubbe@users.noreply.github.com>
2026-03-17 15:46:47 +0100

    Add files via upload

---
+++ Jup_sound.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Jup_sound : AudioStreamPlayer3D
+{
+	[Export] Player player;
+	public override void _Ready()
+	{
+		player.Jump += OnPlayerJump;
+	}
+
+
+    private void OnPlayerJump(object sender, EventArgs e)
+    {
+        Play();
+    }
+}
